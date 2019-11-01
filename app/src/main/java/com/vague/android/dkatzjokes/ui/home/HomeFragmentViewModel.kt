95a2a8113a5831685e724792c7666eeb3d91567d
Vague <vaguemail369@gmail.com>
2019-11-01 23:59:25 +0100

    Remove encapsulation for dataset variable

--- app/src/main/java/com/vague/android/dkatzjokes/ui/home/HomeFragmentViewModel.kt
+++ app/src/main/java/com/vague/android/dkatzjokes/ui/home/HomeFragmentViewModel.kt
@@ -1,16 +1,11 @@
 package com.vague.android.dkatzjokes.ui.home
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.vague.android.dkatzjokes.data.Repository
-import com.vague.android.dkatzjokes.data.model.Joke
 
 class HomeFragmentViewModel(repository: Repository) : ViewModel() {
 
-    private val _dataset = repository.getAllJokes()
-    val dataset: LiveData<List<Joke>>
-        get() = _dataset
-
+    val dataset = repository.getAllJokes()
 
     companion object {
         const val TAG = "HomeFragmentViewModel_Timber"
