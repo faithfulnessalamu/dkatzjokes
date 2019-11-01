04c04294769d0ebb10e937caaaced2c2b4304039
Vague <vaguemail369@gmail.com>
2019-11-01 23:15:37 +0100

    Create interfaces for each data source

--- app/src/main/java/com/vague/android/dkatzjokes/data/DefaultRepository.kt
+++ app/src/main/java/com/vague/android/dkatzjokes/data/DefaultRepository.kt
@@ -7,9 +7,7 @@ import com.vague.android.dkatzjokes.data.source.remote.RemoteDataSource
 
 class DefaultRepository(val localDataSource: LocalDataSource, val remoteDataSource: RemoteDataSource) : Repository {
 
-    override fun getAllJokes(): LiveData<List<Joke>> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun getAllJokes(): LiveData<List<Joke>> = localDataSource.getAllJokes()
 
     override fun getTenNewJokes(): LiveData<List<Joke>> {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
