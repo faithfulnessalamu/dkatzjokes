7501e3dee33d6448e0a3420ad824ee807d1f2fd4
Vague <vaguemail369@gmail.com>
2019-11-01 20:51:54 +0100

    Initialize home fragment with a layout

--- app/src/main/java/com/vague/android/dkatzjokes/ui/home/HomeFragment.kt
+++ app/src/main/java/com/vague/android/dkatzjokes/ui/home/HomeFragment.kt
@@ -1,7 +1,24 @@
 package com.vague.android.dkatzjokes.ui.home
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.vague.android.dkatzjokes.databinding.FragHomeBinding
+import timber.log.Timber
 
 class HomeFragment : Fragment() {
 
+    private val homeTimber = Timber.tag(TAG)
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val binding = FragHomeBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    companion object {
+        const val TAG = "HomeFragment"
+    }
 }
