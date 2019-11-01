f6e24b92364c91e9c09124b619a831a30bff76ac
Vague <vaguemail369@gmail.com>
2019-11-01 21:30:26 +0100

    Update recyclerview adapter

--- app/src/main/java/com/vague/android/dkatzjokes/ui/home/HomeFragmentAdapter.kt
+++ app/src/main/java/com/vague/android/dkatzjokes/ui/home/HomeFragmentAdapter.kt
@@ -1,10 +1,14 @@
 package com.vague.android.dkatzjokes.ui.home
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.chip.Chip
+import com.vague.android.dkatzjokes.R
 import com.vague.android.dkatzjokes.data.model.Joke
 
 class HomeFragmentAdapter : ListAdapter<Joke, HomeFragmentViewHolder>(
@@ -19,15 +23,24 @@ class HomeFragmentAdapter : ListAdapter<Joke, HomeFragmentViewHolder>(
     }
 ) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeFragmentViewHolder {
-
+        val inflatedView = LayoutInflater.from(parent.context).inflate(R.layout.layout_joke_item, parent, false)
+        return HomeFragmentViewHolder(inflatedView)
     }
 
     override fun onBindViewHolder(holder: HomeFragmentViewHolder, position: Int) {
-        holder.bind()
+        holder.bind(getItem(position))
     }
 }
 
 class HomeFragmentViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
-    fun bind() {}
+    val txtJokeSetup = view.findViewById<TextView>(R.id.txt_joke_setup)
+    val txtJokePunchline = view.findViewById<TextView>(R.id.txt_joke_punchline)
+    val txtJokeType = view.findViewById<Chip>(R.id.chip_joke_type)
+
+    fun bind(joke: Joke) {
+        txtJokeSetup.text = joke.setup
+        txtJokePunchline.text = joke.punchline
+        txtJokeType.text = joke.type
+    }
 }
