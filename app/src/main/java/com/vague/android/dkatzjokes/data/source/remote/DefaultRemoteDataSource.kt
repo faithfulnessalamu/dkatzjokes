b35a55c987028a3fd6a10328b08d337481d3941b
Vague <vaguemail369@gmail.com>
2019-11-02 17:50:38 +0100

    Update livedata when api returns

--- app/src/main/java/com/vague/android/dkatzjokes/data/source/remote/DefaultRemoteDataSource.kt
+++ app/src/main/java/com/vague/android/dkatzjokes/data/source/remote/DefaultRemoteDataSource.kt
@@ -1,6 +1,7 @@
 package com.vague.android.dkatzjokes.data.source.remote
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.vague.android.dkatzjokes.data.model.Joke
 import retrofit2.Call
 import retrofit2.Callback
@@ -10,21 +11,28 @@ import retrofit2.Response
 class DefaultRemoteDataSource(private val dkatzApiService: DkatzApiService) : RemoteDataSource {
 
     override fun getTenNewJokes(): LiveData<ApiResult> {
+        val result = MutableLiveData<ApiResult>(ApiResult.Success(emptyList()))
 
         dkatzApiService.getTenNewJokes().apply {
             enqueue(object : Callback<List<Joke>> {
                 override fun onFailure(call: Call<List<Joke>>, t: Throwable) {
-                    ApiResult.Failure(t.message ?: "Cause not specified")
+                    result.value = ApiResult.Failure(t.message ?: "Cause not specified")
                 }
 
                 override fun onResponse(call: Call<List<Joke>>, response: Response<List<Joke>>) {
-                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                    if (response.isSuccessful) {
+                        val list = response.body()
+                        result.value = ApiResult.Success(list ?: emptyList())
+                    } else {
+                        result.value = ApiResult.Failure("Unsuccessful api call.")
+                    }
                 }
             })
+
             execute()
         }
 
-
+        return result
     }
 
     companion object {
