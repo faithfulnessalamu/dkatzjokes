1b60f2bfa842aafc73d193afd0cf2ad6ad5b8007
Vague <vaguemail369@gmail.com>
2019-11-02 19:11:18 +0100

    Use a REPLACE conflict strategy in jokes dao

--- app/src/main/java/com/vague/android/dkatzjokes/data/source/local/JokesDao.kt
+++ app/src/main/java/com/vague/android/dkatzjokes/data/source/local/JokesDao.kt
@@ -3,6 +3,7 @@ package com.vague.android.dkatzjokes.data.source.local
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.vague.android.dkatzjokes.data.model.Joke
 
@@ -12,6 +13,6 @@ interface JokesDao {
     @Query("SELECT * FROM jokes")
     fun getAllJokes(): LiveData<List<Joke>>
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun saveJokes(jokes: List<Joke>)
 }
