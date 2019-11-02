b825b8400bb329748d817d6faec10a4d8f2eec04
Vague <vaguemail369@gmail.com>
2019-11-02 07:32:24 +0100

    Make Joke model a Room entity Create the JokesDao

--- app/src/main/java/com/vague/android/dkatzjokes/data/model/Joke.kt
+++ app/src/main/java/com/vague/android/dkatzjokes/data/model/Joke.kt
@@ -1,7 +1,11 @@
 package com.vague.android.dkatzjokes.data.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "jokes")
 data class Joke(
-    val id: String,
+    @PrimaryKey val id: String,
     val type: String,
     val setup: String,
     val punchline: String
---
+++ app/src/main/java/com/vague/android/dkatzjokes/data/source/local/JokesDao.kt
@@ -0,0 +1,17 @@
+package com.vague.android.dkatzjokes.data.source.local
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.vague.android.dkatzjokes.data.model.Joke
+
+@Dao
+interface JokesDao {
+
+    @Query("SELECT * FROM jokes")
+    fun getAllJokes(): LiveData<List<Joke>>
+
+    @Insert
+    suspend fun saveJokes(jokes: List<Joke>)
+}
