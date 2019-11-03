5d98ad84162d5ddd0bc7cf1a74ce099e32ffe40a
Vague <vaguemail369@gmail.com>
2019-11-03 17:01:50 +0100

    Refresh when fab is clicked

--- app/src/main/java/com/vague/android/dkatzjokes/ui/home/HomeFragment.kt
+++ app/src/main/java/com/vague/android/dkatzjokes/ui/home/HomeFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearSnapHelper
+import com.vague.android.dkatzjokes.R
+import com.vague.android.dkatzjokes.data.source.remote.ApiResult
 import com.vague.android.dkatzjokes.databinding.FragHomeBinding
 import com.vague.android.dkatzjokes.di.DependencyManager
 import timber.log.Timber
@@ -30,7 +34,7 @@ class HomeFragment : Fragment() {
 
 
         setupRecyclerView()
-        //setupSwipeRefresh()
+        setupFab()
 
         binding.viewModel?.getSavedJokes()?.observe(viewLifecycleOwner, Observer {
             if (it == null) return@Observer
@@ -38,13 +42,14 @@ class HomeFragment : Fragment() {
             (binding.recyclerviewJokesHome.adapter as HomeFragmentAdapter)
                 .submitList(it.reversed())
             binding.recyclerviewJokesHome.adapter?.notifyItemInserted(0)
+            binding.recyclerviewJokesHome.smoothScrollToPosition(0)
         })
 
         return binding.root
     }
 
-    /*private fun setupSwipeRefresh() {
-        binding.swipeRefreshLayout.setOnRefreshListener {
+    private fun setupFab() {
+        binding.appFab.setOnClickListener {
             binding.viewModel?.setRefreshing(true)
 
             binding.viewModel?.getNewJokes()?.observe(viewLifecycleOwner,
@@ -54,19 +59,40 @@ class HomeFragment : Fragment() {
                         is ApiResult.Success -> {
                             if (it.data.isNotEmpty()) {
                                 homeTimber.d("Updating adapter")
+                                makeToast("  Done  ")
                                 binding.viewModel?.setRefreshing(false)
                                 binding.viewModel?.saveJokes(it.data)
                             }
                         }
                         is ApiResult.Failure -> {
                             binding.viewModel?.setRefreshing(false)
+                            makeToast("  Network not available  ")
                             homeTimber.e(it.message)
                         }
                     }
                 }
             )
         }
-    }*/
+
+
+        binding.viewModel?.isRefreshing?.observe(viewLifecycleOwner, Observer {
+            if (it) {
+                makeToast("  Updating Jokes  ")
+                // Start animation
+                val rotation = AnimationUtils.loadAnimation(requireActivity(), R.anim.infinite_rotate).apply {
+                    fillAfter = true
+                }
+                binding.appFab.startAnimation(rotation)
+            } else {
+                binding.appFab.clearAnimation()
+            }
+        })
+    }
+
+    private fun makeToast(s: String) {
+        Toast.makeText(context, s, Toast.LENGTH_SHORT).show()
+    }
+
 
     private fun setupRecyclerView() {
         homeTimber.d("setupRecyclerView Called")
